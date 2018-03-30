29153b47a2c0cd76d7b91607753c8c25c04d7888
Marty Stepien <marty.stepien@gmail.com>
2018-03-30 17:16:23 +0100

    main script clean up

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 function initMap() {
-	console.log("Init map at London zoo")
-	var zoo = new google.maps.LatLng(51.535311, -0.153296)
-	var myOptions = {
+	console.log('Init map at London zoo');
+
+	// Draw map centred in London Zoo
+	var zoo = new google.maps.LatLng(51.535311, -0.153296);
+	var mapOptions = {
 		zoom: 12,
 		center: zoo,
-		mapTypeId: google.maps.MapTypeId.ROADMAP, 
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		styles: stylesArray
 	};
-	// Draw the map
-	var map = new google.maps.Map(document.getElementById("map"), myOptions);
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	// Add custom penguin marker pin in London Zoo
 	var infowindow = new google.maps.InfoWindow({
-		content: '<p>Sqawk!</p>'
+		content: '<p style="font-size: 16px; margin-bottom: 0; padding: 10px 0 5px">Sqawk!</p>'
 	});
 	var marker = new google.maps.Marker({
 		position: zoo,
@@ -23,45 +26,50 @@ function initMap() {
 		infowindow.open(map, marker);
 	});
 
+	// Allow address autocomplete for user input - Google Places API Web Service
 	var input = document.getElementById('start');
 	var autocomplete = new google.maps.places.Autocomplete(input);
 
+	// Find directions to the zoo - Google Maps Directions API
 	var directionsService = new google.maps.DirectionsService();
 	var directionsDisplay = new google.maps.DirectionsRenderer();
 	directionsDisplay.setMap(map);
-	console.log(zoo);
-
-	var findRoute = function () {
-		if (document.getElementById('start').value == '') {
-			window.alert('You need to specify the start!');
-			return;
-		}
-		marker.setMap(null);
-		calcRoute(directionsService, directionsDisplay, zoo);
-	};
-
-	var updateTravelMode = function () {
-		if (document.getElementById('start').value == '') {
-			return;
-		}
-		calcRoute(directionsService, directionsDisplay, zoo);
-	};
 
 	document.getElementById('find-start').addEventListener('click', findStart);
 	document.getElementById('get-directions').addEventListener('click', findRoute);
 	document.getElementById('mode').addEventListener('change', updateTravelMode);
 
+	function findRoute() {
+		console.log('Check if user\'s location not empty');
+		if (document.getElementById('start').value == '') {
+			window.alert('You need to specify your location!');
+			return;
+		}
+		console.log('Find directions to the zoo');
+		marker.setMap(null);
+		calcRoute(directionsService, directionsDisplay, zoo);
+	}
 
+	function updateTravelMode() {
+		console.log('Check if user\'s location not empty');
+		if (document.getElementById('start').value == '') {
+			return;
+		}
+		console.log('Update travel mode');
+		marker.setMap(null);
+		calcRoute(directionsService, directionsDisplay, zoo);
+	}
 }
 
 function calcRoute(directionsService, directionsDisplay, end) {
-	console.log("calcRoute hey!");
+	console.log('Calculate route');
 	var selectedMode = document.getElementById('mode').value;
 	var request = {
 		origin: document.getElementById('start').value,
 		destination: end,
 		travelMode: google.maps.TravelMode[selectedMode]
 	};
+
 	directionsService.route(request, function (response, status) {
 		if (status == 'OK') {
 			directionsDisplay.setDirections(response);
@@ -72,31 +80,30 @@ function calcRoute(directionsService, directionsDisplay, end) {
 }
 
 function findStart() {
-	console.log("start");
-
+	console.log('Find user\'s geolocation');
 	if (!navigator.geolocation) {
-		console.log("geolocation not supported");
-		window.alert("Uuh-ooh, your browser doesn't support the Geolocation API");
+		console.log('geolocation not supported');
+		window.alert('Uuh-ooh, your browser doesn\'t support the Geolocation API');
 		return;
 	}
 
 	navigator.geolocation.getCurrentPosition(function (position) {
-			var geocoder = new google.maps.Geocoder();
-			geocoder.geocode({
-					"location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-				},
-				function (results, status) {
-					if (status === google.maps.GeocoderStatus.OK) {
-						document.getElementById('start').value = results[0].formatted_address;
-					} else {
-						window.alert("Unable to find your address")
-					}
-				});
-		},
-		function (positionError) {
-			console.log("Error: " + positionError.message);
-		}, {
-			enableHighAccuracy: true,
-			// timeout: 20 * 1000 // 20 seconds
-		});
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+				'location': new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+			},
+			function (results, status) {
+				if (status === google.maps.GeocoderStatus.OK) {
+					document.getElementById('start').value = results[0].formatted_address;
+				} else {
+					window.alert('Unable to find your location');
+				}
+			});
+	},
+	function (positionError) {
+		console.log('Error: ' + positionError.message);
+	}, {
+		enableHighAccuracy: true,
+		timeout: 20 * 1000 // 20 seconds
+	});
 }
